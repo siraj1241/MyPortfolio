10469b93e455b57aee5eb6fd41c2eb03864ad2c0
siraj1241 <shaiksiraj1241@gmail.com>
2026-01-23 18:27:17 +0530

    issue resolved for learning

--- MyPortfolio/Controllers/AccountController.cs
+++ MyPortfolio/Controllers/AccountController.cs
@@ -50,8 +50,8 @@ namespace MyPortfolio.Controllers
         public async Task<IActionResult> Login(User model)
         {
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == model.Username);
-
-            if (user != null && user.Password == model.Password) // Use hashed password verification in production
+            model.Password = MyPortfolio.Models.User.HashPassword(model.Password);
+            if (user != null && model.Password == user.Password)
             {
                 var claims = new List<Claim>
         {
